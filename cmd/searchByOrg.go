8d6766749b6bf2f162bfe5c40ebd00816882f54b
DipanshuOjha <dipanshuojha12@gmail.com>
2025-06-12 06:16:00 +0530

    created SearchOrg to search repos for a perticular organisation

---
+++ cmd/searchByOrg.go
@@ -0,0 +1,108 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/DipanshuOjha/cobraclip/internal/config"
+	"github.com/google/go-github/v62/github"
+	"github.com/spf13/cobra"
+)
+
+var (
+	org string
+	cnt int
+)
+
+// searchByOrgCmd represents the searchByOrg command
+var searchByOrgCmd = &cobra.Command{
+	Use:   "Search Repository",
+	Short: "Search Github Repository by Organisation",
+	Long: `
+	   parameter that are to search repositories within an organisation
+	                --org or -o name of organisation 
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg, err := config.LoadConfig()
+		if err != nil {
+			fmt.Println("Not able to config the user token i guess you should login using cobraclip login \n %w\n", err)
+			return
+		}
+
+		client, err := config.GetGithubClient(cfg)
+
+		if err != nil {
+			fmt.Println("Not able to create github client check out \n %w\n", err)
+			return
+		}
+
+		ctx := context.Background()
+
+		data := &github.RepositoryListByOrgOptions{
+			ListOptions: github.ListOptions{PerPage: 100},
+		}
+
+		var AllRepos []*github.Repository
+
+		for {
+			repos, resp, err := client.Repositories.ListByOrg(ctx, org, data)
+			if err != nil {
+				fmt.Printf("Error listing repositories: %v\n", err)
+				if resp != nil {
+					fmt.Printf("GitHub API response: %s\n", resp.Status)
+				}
+				return
+			}
+			AllRepos = append(AllRepos, repos...)
+			if resp.NextPage == 0 {
+				break
+			}
+			data.Page = resp.NextPage
+		}
+
+		if len(AllRepos) == 0 {
+			fmt.Printf("No repositories found for organization: %s\n", org)
+			return
+		}
+
+		fmt.Printf("Found %d repositories for %s\n", len(AllRepos), org)
+		for i, repo := range AllRepos {
+			fmt.Printf("\n%d. %s\n", i+1, *repo.FullName)
+			desc := "No description"
+			if repo.Description != nil {
+				desc = *repo.Description
+			}
+			fmt.Printf("   Description: %s\n", desc)
+			fmt.Printf("   Stars: %d\n", *repo.StargazersCount)
+			fmt.Printf("   URL: %s\n", *repo.HTMLURL)
+			time.Sleep(time.Millisecond * 500)
+			if i+1 == cnt {
+				break
+			}
+		}
+
+		fmt.Println("That all Repos")
+
+	},
+}
+
+func init() {
+	repoCmd.AddCommand(searchByOrgCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// searchByOrgCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// searchByOrgCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	searchByOrgCmd.Flags().StringVarP(&org, "org", "o", "", "Name of Organisation(Required)")
+	searchByOrgCmd.Flags().IntVarP(&cnt, "cnt", "c", 100000000, "Count of repos")
+	searchByOrgCmd.MarkFlagRequired("org")
+}
